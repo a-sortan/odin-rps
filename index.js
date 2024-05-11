8a3f05f4eae58c50c11d7bde093e7f762eccddec
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-11 18:42:35 +0200

    Create a function that returns the player choice

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
-const GAME_CHOICES = ["rock", "paper", "scissors"]
+const GAME_CHOICES = ["rock", "paper", "scissors"];
 function getComputerChoice() {
   return GAME_CHOICES[Math.floor(Math.random() * 3)];
+}
+
+function getHumanChoice() {
+  let choice = prompt("Chose one: Rock, Paper Scissors");
+  let choiceIndex = GAME_CHOICES.indexOf(choice.toLowerCase())
+  if(choiceIndex > -1) {
+    return GAME_CHOICES[choiceIndex]
+  } 
 }
