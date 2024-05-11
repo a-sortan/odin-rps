992ad4d1b241d293965591584e7d3d0f5337c378
a-sortan <1005985+a-sortan@users.noreply.github.com>
2024-05-11 19:10:26 +0200

    Create function for a single round

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const GAME_CHOICES = ["rock", "paper", "scissors"];
+
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   return GAME_CHOICES[Math.floor(Math.random() * 3)];
 }
@@ -10,3 +14,54 @@ function getHumanChoice() {
     return GAME_CHOICES[choiceIndex]
   } 
 }
+
+function playRound(humanChoice, computerChoice) {
+  let msg = "";
+  switch (humanChoice) {
+    case "rock":
+      if(computerChoice === "paper") {
+        msg = "You loose! Paper covers Rock!";
+        computerScore++;
+      } else if (computerChoice === "scissors") {
+        msg = "You win! Rock crushes Scissors!";
+        humanScore++;
+      } else {
+        msg = "It's a tie!";
+        computerScore++;
+        humanScore++;
+      }
+      break;
+    case "paper":
+      if(computerChoice === "scissors") {
+        msg = "You loose! Scissors cuts Paper!";
+        computerScore++;
+      } else if (computerChoice === "rock") {
+        msg = "You win! Paper covers Rock!";
+        humanScore++;
+      } else {
+        msg = "It's a tie!";
+        computerScore++;
+        humanScore++;
+      }
+      break;
+    case "scissors":
+      if(computerChoice === "rock") {
+        msg = "You loose! Rock crushes Scissors!";
+        computerScore++;
+      } else if (computerChoice === "paper") {
+        msg = "You win! Scissors cuts paper!";
+        humanScore++;
+      } else {
+        msg = "It's a tie!";
+        computerScore++;
+        humanScore++;
+      }
+      break;
+  }
+  return `${msg} Your score: ${humanScore} Computer score: ${computerScore}`;
+}
+
+let humanSelection = getHumanChoice();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(humanSelection, computerSelection));
